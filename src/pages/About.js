7d43494805e1b40834213635f6d4b84177ed6713
lupenq <skinilya@gmail.com>
2020-04-03 16:12:05 +0300

    add navigation

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const About = () => (
+    <div className="jumbotron">
+      <div className="container">
+        <h1 className="display-4">Лучшее React приложение</h1>
+        <p className="lead">
+          Версия 1.0.1
+        </p>
+      </div>
+    </div>
+)
